51c9849bb808915319a1311facb7074fcfce21ed
panthita <panthita029@gmail.com>
2025-05-03 20:50:44 +0700

    Check type mountain bike 1 or 2

--- sizeCal.js
+++ sizeCal.js
@@ -1,53 +1,53 @@
 function sizeCalculate() {
-  let t = document.querySelector('input[name="btypeRad"]:checked').value;
-  let h = (document.getElementById("myHeight").value).trim();
-  let i = (document.getElementById("myInseam").value).trim();
-  let size;
-  //xxxxx calculate bike size xxxxx
-  if(t == 2){//road bike
-    if(h >= 155 && h <= 163){
+   let t = document.querySelector('input[name="btypeRad"]:checked').value; // 1 หรือ 2
+   let h = (document.getElementById("myHeight").value).trim();
+   let i = (document.getElementById("myInseam").value).trim();
+   let size;
+ 
+   // คำนวณขนาดจักรยานตามประเภทจักรยาน (Mountain bike = 1, Road bike = 2)
+   if (t == "2") { // Road bike (เลือก 2)
+     if (h >= 155 && h <= 163) {
        size = '50';
-    }else if(h >= 164 && h <= 169){
-       if(i >= 74 && h <= 77){
-        size = '52';
-       }else if(i > 77){
-        size = '54';
-       }else{
-        size = '50';
+     } else if (h >= 164 && h <= 169) {
+       if (i >= 74 && i <= 77) {
+         size = '52';
+       } else if (i > 77) {
+         size = '54';
+       } else {
+         size = '50';
        }
-    }else if(h >= 170 && h <= 179){
-       if(i >= 78 && h <= 82){
-        size = '54';
-       }else if(i > 82){
-        size = '56';
-       }else{
-        size = '52';
+     } else if (h >= 170 && h <= 179) {
+       if (i >= 78 && i <= 82) {
+         size = '54';
+       } else if (i > 82) {
+         size = '56';
+       } else {
+         size = '52';
        }
-    }else if(h >= 180){
-       if(i >= 78){
-        size = '56';
-       }else{
-        size = '54';
+     } else if (h >= 180) {
+       if (i >= 78) {
+         size = '56';
+       } else {
+         size = '54';
        }
-    }else{
+     } else {
        size = 'N/A';
-    }
-  }else{//mountain bike
-    if(h >= 155 && h <= 163){
-      size = 'S';
-   }else if(h >= 164 && h <= 169){
-      size = 'M';
-   }else if(h >= 170 && h <= 179){
-      size = 'L';
-   }else if(h >= 180){
-      size = 'XL';
-   }else{
-      size = 'N/A';
+     }
+   } else { // Mountain bike (เลือก 1)
+     if (h >= 155 && h <= 163) {
+       size = 'S';
+     } else if (h >= 164 && h <= 169) {
+       size = 'M';
+     } else if (h >= 170 && h <= 179) {
+       size = 'L';
+     } else if (h >= 180) {
+       size = 'XL';
+     } else {
+       size = 'N/A';
+     }
    }
-  }
-  //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-  return size;
-}
-
-
+ 
+   return size;
+ }
+ 
+ 
